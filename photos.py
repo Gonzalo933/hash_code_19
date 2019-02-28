79a064365299dfb21083d405b6ddafd50dddc467
Gonzalo <gonzalo.hernandez.1293@gmail.com>
2019-02-28 22:36:11 +0100

    last commit

--- photos.py
+++ photos.py
@@ -9,11 +9,20 @@ def is_horizontal(photo):
     return photo["orientation"] == "H"
 
 
+def is_vertical(photo):
+    return not photo["orientation"] == "H"
+
+
 def write_file(photos_list):
     with open(sys.argv[-1] + ".out", "w") as f:
         f.write(str(len(photos_list)) + "\n")
         for photo in photos_list:
-            f.write(str(photo["id"]) + "\n")
+            if type(photo["id"]) == list:
+                for id in photo["id"]:
+                    f.write(str(id) + " ")
+                f.write("\n")
+            else:
+                f.write(str(photo["id"]) + "\n")
 
 
 def main(file_path):
@@ -25,14 +34,67 @@ def main(file_path):
         all_tags += p["tags"]
     unique, counts = np.unique(all_tags, return_counts=True)
     tags_count = dict(zip(unique, counts))
-    photos_without_vertical = list(filter(is_horizontal, photos_list))
-    most_tags = np.argmax([len(photo["tags"]) for photo in photos_list])
+    horizontal_photos = list(filter(is_horizontal, photos_list))
+    vertical_photos = list(filter(is_vertical, photos_list))
+    if not len(vertical_photos) % 2:
+        vertical_photos = vertical_photos[1:]
+    new_horizontals = merge_vertical_photos(vertical_photos.copy(), 0)
+    photos_list = horizontal_photos + new_horizontals
+    grouped_photos = []
+    group_size = 100
+    for i in range(0, len(photos_list), group_size):
+        grouped_photos.append(photos_list[i : i + group_size])
+    final_solution = []
+    current_score = 0
+    group_num = 0
+    for group in grouped_photos:
+        most_tags = np.argmax([len(photo["tags"]) for photo in group])
+        solution, solution_score = branch_and_bound(group.copy(), most_tags)
+        final_solution += solution
+        current_score += solution_score
+        group_num += 1
+        print(f"group={group_num}/{len(grouped_photos)}")
+        print(f"Solution score {current_score}")
     print(f"most tags={most_tags}")
-    solution = branch_and_bound(photos_without_vertical.copy(), most_tags)
-    write_file(solution)
+    write_file(final_solution)
     import pdb
 
     pdb.set_trace()
+
+
+def merge_vertical_photos(vertical_photos, best_index):
+    solution = []
+    flag = True
+    while len(vertical_photos) > 0:
+        scores = []
+        vertical_first = vertical_photos.pop(best_index)
+        flag = True
+        for index, photo in enumerate(vertical_photos):
+            current_score = score(vertical_first, photo)
+            if current_score == 0:
+                # MERGE
+                solution.append(merge_photos(vertical_first, photo))
+                vertical_photos.pop(index)
+                best_index = 0
+                flag = False
+                break
+            scores.append(current_score)
+        # Choose best score
+        if flag and len(vertical_photos) > 0:
+            best_index = np.argmin(scores)
+            solution.append(merge_photos(vertical_first, photo))
+            vertical_photos.pop(best_index)
+            best_index = 0
+        # print(f"Score={current_score}, Photo={len(solution)}/{(total_photos)}")
+    return solution
+
+
+def merge_photos(photo_1, photo_2):
+    return {
+        "tags": photo_1["tags"] + list(set(photo_2["tags"]) - set(photo_1["tags"])),
+        "id": [photo_1["id"], photo_2["id"]],
+        "orientation": "H",
+    }
 
 
 def score(photo1, photo2):
@@ -56,19 +118,14 @@ def branch_and_bound(photos_list, best_index):
         best_index = np.argmax(scores)
         current_score += np.max(scores)
         solution.append(photos_list.pop(best_index))
-        print(f"Score={current_score}, Photo={len(solution)}/{(total_photos)}")
+        # print(f"Score={current_score}, Photo={len(solution)}/{(total_photos)}")
         # if len(solution) >= 500:
         #    break
-    return solution
+    return solution, current_score
 
 
 def read_photo(photo_string, i):
-    return {
-        "orientation": photo_string[0],
-        "selected": False,
-        "tags": photo_string[2:],
-        "id": i,
-    }
+    return {"orientation": photo_string[0], "tags": photo_string[2:], "id": i}
 
 
 def read_file(file_path):
